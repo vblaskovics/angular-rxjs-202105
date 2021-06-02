65880ff61cac034cb2ca9a85642ec00cbe0491ea
Viktor Blaskovics <viktor.blaskovics@gmail.com>
2021-06-02 15:00:14 +0200

    Add message service functions

--- src/app/user/users.service.ts
+++ src/app/user/users.service.ts
@@ -1,10 +1,25 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, Subject } from 'rxjs';
+import { User } from './user.model';
 
 /**
  * UserService manages our current user
  */
 @Injectable()
 export class UsersService {
-  
+    // Usage:
+    // Write:
+    // let user1 = new User('Bob');
+    // UserService.currentUser.next(user1);
+    // Read:
+    // UserService.currentUser.subscribe((user)=>{
+        // console.log(user)
+    // })
+    currentUser: Subject<User> = new BehaviorSubject<User>(null);
+
+    public setCurrentUser(newUser: User) {
+        this.currentUser.next(newUser);
+    }
+
 }
 
