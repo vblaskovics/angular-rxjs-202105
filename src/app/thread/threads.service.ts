33126356b6a503aab5c7d9cd5f9beb95ddda42df
Viktor Blaskovics <viktor.blaskovics@gmail.com>
2021-06-02 16:01:08 +0200

    Add threads handling

--- src/app/thread/threads.service.ts
+++ src/app/thread/threads.service.ts
@@ -1,8 +1,31 @@
 import { Injectable } from '@angular/core';
 import * as _ from 'lodash';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { Message } from '../message/message.model';
+import { MessagesService } from '../message/messages.service';
+import { Thread } from './thread.model';
 
 @Injectable()
 export class ThreadsService {
-  constructor() {
+  threads: Observable<{ [key:string]: Thread }>;
+
+  constructor(public messagesService: MessagesService) {
+    this.threads = messagesService.messages.pipe(
+      map((messages: Message[]) => {
+        const threads: { [key:string] : Thread } = {};
+
+        messages.map((message: Message)=>{
+          threads[message.thread.id] = threads[message.thread.id] || message.thread;
+
+          const messsagesThread: Thread = threads[message.thread.id]
+          if(!messsagesThread.lastMessage || messsagesThread.lastMessage.sentAt < message.sentAt) {
+            messsagesThread.lastMessage = message;
+          }
+        })
+
+        return threads;
+      })
+    )
   }
 }
