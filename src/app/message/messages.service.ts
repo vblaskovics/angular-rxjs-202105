9d81398309423f3e27b20a3c08c0846d7f5c87c0
Viktor Blaskovics <viktor.blaskovics@gmail.com>
2021-06-02 16:37:55 +0200

    Add markThreadAsRead

--- src/app/message/messages.service.ts
+++ src/app/message/messages.service.ts
@@ -1,14 +1,14 @@
-import { Injectable } from '@angular/core';
-import { Observable, Subject } from 'rxjs';
-import { Thread } from '../thread/thread.model';
-import { User } from '../user/user.model';
-import { Message } from './message.model';
-import { filter, scan, publishReplay, refCount, map } from 'rxjs/operators'
+import { Injectable } from "@angular/core";
+import { Observable, Subject } from "rxjs";
+import { Thread } from "../thread/thread.model";
+import { User } from "../user/user.model";
+import { Message } from "./message.model";
+import { filter, scan, publishReplay, refCount, map } from "rxjs/operators";
 
 const initialMessages: Message[] = [];
 
 interface IMessageOperation extends Function {
-  (messages: Message[]): Message[]
+  (messages: Message[]): Message[];
 }
 
 @Injectable()
@@ -17,6 +17,7 @@ export class MessagesService {
   messages: Observable<Message[]>;
   updates: Subject<any> = new Subject<any>();
   create: Subject<Message> = new Subject<Message>();
+  markThreadAsRead: Subject<any> = new Subject<any>();
 
   constructor() {
     this.messages = this.updates.pipe(
@@ -25,32 +26,45 @@ export class MessagesService {
       }, initialMessages),
       publishReplay(1),
       refCount()
-    )
+    );
 
-    this.create.pipe(
-      map(
-        (message: Message): IMessageOperation => {
+    this.create
+      .pipe(
+        map((message: Message): IMessageOperation => {
           return (messages: Message[]) => {
             return messages.concat(message);
-          }
-        }
+          };
+        })
       )
-    ).subscribe(this.updates);
+      .subscribe(this.updates);
 
     this.newMessages.subscribe(this.create);
 
+    this.markThreadAsRead
+      .pipe(
+        map((thread: Thread) => {
+          return (messages: Message[]) => {
+            return messages.map((message: Message) => {
+              if (message.thread.id === thread.id) {
+                message.isRead = true;
+              }
+              return message;
+            });
+          };
+        })
+      )
+      .subscribe(this.updates);
   }
 
   addMessage(message: Message): void {
     this.newMessages.next(message);
   }
 
-  messagesForThreadUser(thread: Thread, user:User): Observable<Message>{
+  messagesForThreadUser(thread: Thread, user: User): Observable<Message> {
     return this.newMessages.pipe(
-      filter((message:Message)=>{
+      filter((message: Message) => {
         return message.thread.id === thread.id && message.author.id !== user.id;
       })
     );
   }
-
 }
