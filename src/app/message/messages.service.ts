49184b849580ea4e9a84ec445bf91f07faed10fb
Viktor Blaskovics <viktor.blaskovics@gmail.com>
2021-06-02 15:37:41 +0200

    Add message stream

--- src/app/message/messages.service.ts
+++ src/app/message/messages.service.ts
@@ -3,15 +3,31 @@ import { Observable, Subject } from 'rxjs';
 import { Thread } from '../thread/thread.model';
 import { User } from '../user/user.model';
 import { Message } from './message.model';
-import { filter } from 'rxjs/operators'
+import { filter, scan, publishReplay, refCount } from 'rxjs/operators'
 
+const initialMessages: Message[] = [];
+
+interface IMessageOperation extends Function {
+  (messages: Message[]): Message[]
+}
 
 @Injectable()
 export class MessagesService {
   newMessages: Subject<Message> = new Subject<Message>();
+  messages: Observable<Message[]>;
+  updates: Subject<any> = new Subject<any>();
+  create: Subject<Message> = new Subject<Message>();
 
   constructor() {
+    this.messages = this.updates.pipe(
+      scan((messages: Message[], operation: IMessageOperation) => {
+        return operation(messages);
+      }, initialMessages),
+      publishReplay(1),
+      refCount()
+    )
 
+    
   }
 
   addMessage(message: Message): void {
